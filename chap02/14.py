389468059c5119e1f59a429aaab3d3bf56e17224
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-01-23 13:17:53 +0900

    update 14

--- chap02/14.py
+++ chap02/14.py
@@ -5,4 +5,4 @@ path = "./hightemp.txt"
 with open(path, "r", encoding="utf-8") as f:
     datas = f.readlines()
     for i in range(0, end_row):
-        print(datas[i])
+        print(datas[i].replace("\n",""))
