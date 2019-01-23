25202059a9b2274d1d9bb635ad1aed3c02349e72
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-01-23 14:45:25 +0900

    16

---
+++ chap02/16.py
@@ -0,0 +1,16 @@
+import sys
+import math
+args = sys.argv
+# split = int(args[1])
+split = 7
+path = "./hightemp.txt"
+with open(path, "r", encoding="utf-8") as f:
+    datas = f.readlines()
+
+datas_len = len(datas)
+unit = math.ceil(len(datas)/split)
+
+for i, offset in enumerate(range(0, datas_len, unit), 1):
+    with open("16_" + str(i) + ".txt", "w", encoding="utf-8") as out_file:
+        for j in range(offset,offset+unit):
+            out_file.writelines(datas[j])
---
+++ chap02/16.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+read n
+split -n $n hightemp.txt
