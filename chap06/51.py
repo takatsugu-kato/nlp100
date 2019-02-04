ac2c489121843ebff56e74b7270fa36c00a9aec7
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-02-04 10:05:06 +0900

    update 51

--- chap06/51.py
+++ chap06/51.py
@@ -1,12 +1,21 @@
 import re
-with open("./nlp.txt","r",encoding="utf-8") as f:
-    data = f.readlines()
 
-separater = [r"\.",";",":",r"\?","!"]
-pattern = "(" + "|".join(separater)  + ") ([A-Z])"
 
-for line in data:
-    line = line.strip()
-    if (len(line)):
-        result = re.sub(pattern, "\\1\n\\2", line)
-        print ("\n".join(result.split(" ")) + "\n")
+def parse_lines (path):
+    with open(path,"r",encoding="utf-8") as f:
+        data = f.readlines()
+    lines = []
+    separater = [r"\.",";",":",r"\?","!"]
+    pattern = "(" + "|".join(separater)  + ") ([A-Z])"
+    for line in data:
+        line = line.strip()
+        if (len(line)):
+            result = re.sub(pattern, "\\1\n\\2", line)
+            lines.extend(result.split("\n"))
+    return lines
+
+path = "./nlp.txt"
+lines = parse_lines(path)
+
+for line in lines:
+    print ("\n".join(line.split(" ")) + "\n")
