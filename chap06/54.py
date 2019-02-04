0e5431be927eb89384a123bce57c9ae0af97c65d
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-02-04 13:00:03 +0900

    54

---
+++ chap06/54.py
@@ -0,0 +1,29 @@
+from pycorenlp import StanfordCoreNLP
+import xml.etree.ElementTree as ET
+
+path = "./nlp.txt"
+nlp = StanfordCoreNLP('http://localhost:9000')
+
+def parseLines (path):
+    with open(path,"r",encoding="utf-8") as f:
+        data = f.readlines()
+    lines = data
+    return lines
+
+def getAnnotateXML (text):
+    output = nlp.annotate(text, properties={
+        'annotators': 'lemma',
+        'outputFormat': 'xml'
+    })
+    return output
+
+lines = parseLines(path)
+xml = getAnnotateXML(lines[9])
+
+
+root = ET.fromstring(xml)
+for sentences in root[0]:
+    for sentence in sentences:
+        for tokens in sentence:
+            for token in tokens:
+                print (token.find('word').text + "\t" + token.find('lemma').text + "\t" + token.find('POS').text)
