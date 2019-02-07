40a31165141476f8e5427da008de87ef4fc42961
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-02-07 13:48:50 +0900

    Revert "63"
    This reverts commit e6d4d5d3dbc992e0e96d286742565d322369c75c.

--- chap07/63.py
+++ chap07/63.py
@@ -1,19 +1,34 @@
+import redis
 import gzip
-import pymongo
-from pymongo import MongoClient
-import datetime
 import json
 
-client = MongoClient('localhost', 27017)
-
-db = client['nlp100']
-collection = db['artist']
-
-with gzip.open("./artist.json.gz","rt",encoding="utf-8") as fi:
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+with gzip.open("./artist.json.gz", "rt", encoding="utf-8") as fi:
     for line in fi:
-        json_line = json.loads(line)
-        result = collection.insert_one(json_line)
-collection.create_index([('name', pymongo.ASCENDING)])
-collection.create_index([('aliases.name', pymongo.ASCENDING)])  
-collection.create_index([('tags.value', pymongo.ASCENDING)])
-collection.create_index([('rating.value', pymongo.ASCENDING)])
+        d = json.loads(line)
+        values = d.get("tags","None")
+        if not values == "None":
+            for value in values:
+                r.rpush(d["name"] + "\t" + str(d["id"]), str(value["count"]) + "\t" + value["value"])
+        else:
+            r.rpush(d["name"] + "\t" + str(d["id"]), "None")
+
+def join_count_name(text):
+    count, name = text.split("\t")
+    return name + "[" + str(count) + "]"
+
+
+print ("アーティスト名を入力")
+artist = input('>> ')
+
+results = r.keys(artist + '\t*')
+if results:
+    for key in results:
+        name, id = key.decode().split("\t")
+        tags = r.lrange(key,0,-1)
+        if tags[0].decode() == "None":
+            print (name + "(" + id + "): None")
+        else:
+            print (name + "(" + id + "): " + ", ".join(map(lambda x: join_count_name(x.decode()),tags)))
+else:
+    print ("None")
