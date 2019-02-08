4e99f4df695d2ffbc630f44252395b9f624c3d54
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-02-08 14:22:22 +0900

    update 71

--- chap08/nlp71.py
+++ chap08/nlp71.py
@@ -1,9 +1,27 @@
 def isStopWord(word):
-    if word in stop_words:
-        return True
-    else:
-        return False
+    return word.lower() in stop_words
 
-stop_words = ["a", "the", "of", "an"]
+stop_words = "a,did,in,only,then,where\
+all,do,into,onto,there,whether\
+almost,does,is,or,therefore,which\
+also,either,it,our,these,while\
+although,for,its,ours,they,who\
+an,from,just,s,this,whose\
+and,had,ll,shall,those,why\
+any,has,me,she,though,will\
+are,have,might,should,through,with\
+as,having,Mr,since,thus,would\
+at,he,Mrs,so,to,yet\
+be,her,Ms,some,too,you\
+because,here,my,still,until,your\
+been,hers,no,such,ve,yours\
+both,him,non,t,very\
+but,his,nor,than,was\
+by,how,not,that,we\
+can,however,of,the,were\
+could,i,on,their,what\
+d,if,one,them,when"
 
-print (isStopWord("tdhe"))
+stop_words = stop_words.split(",")
+
+print (isStopWord("The"))
